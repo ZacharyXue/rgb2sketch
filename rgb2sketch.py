19b1a774495d0b68ebcf0619310b97c40741c58f
Zach <xuezihao2016@outlook.com>
2022-02-02 18:45:09 +0800

    the first stage

--- rgb2sketch.py
+++ rgb2sketch.py
@@ -11,7 +11,7 @@ import numpy as np
 #     cv2.destroyAllWindows('img')
 
 # 传入图片
-img_path = "./pic/7.jpg"
+img_path = "./pic/13.jpg"
 raw = cv2.imread(img_path)
 # cv2.imshow("img", img)
 
@@ -19,14 +19,18 @@ raw = cv2.imread(img_path)
 gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
 # cv2.imshow("gray", gray)
 
+# 图像取反
 inv = 255 - gray
 # cv2.imshow("inv", inv)
 
+# 高斯滤波
 ksize = 15
 sigma = 50
 blur = cv2.GaussianBlur(inv, ksize=(ksize, ksize), sigmaX=sigma, sigmaY=sigma)
 # cv2.imshow("blur",blur)
 
+# 颜色减淡混合
+# res = 255 * gray / (255 - blur_inv_x) 
 res = cv2.divide(gray, 255 - blur, scale=255)
 # cv2.imshow("res", res)
 
@@ -36,4 +40,5 @@ for s in imgs:
     while cv2.waitKey(100) != ord('n'):
         pass
     cv2.destroyWindow(s)
-cv2.waitKey(0)
+
+cv2.imwrite("./docs/pic/res.png", res)
